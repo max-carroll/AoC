e7e5a6735db86582d9516b06f23e449cb1873ef0
Max Carroll <max.carroll@sky.uk>
2023-12-03 18:28:42 +0000

    tdd seems better

---
+++ 2023/3.spec.ts
@@ -0,0 +1,24 @@
+import assert from "assert";
+import test from "node:test";
+import { LinesToMatrix, getSplit, rawInput } from "./3";
+
+test("should return right number of lines when splitting", () => {
+  const result = getSplit(rawInput);
+  assert.equal(result.length, 140);
+});
+
+test("all lines should be the same length", () => {
+  const result = getSplit(rawInput);
+
+  for (var line of result) {
+    assert.equal(line.length, 140);
+  }
+});
+
+test("should create  a matrix from lines", () => {
+  const result = getSplit(rawInput);
+
+  const matrix = LinesToMatrix(result);
+
+  assert.equal(matrix.length, 140);
+});

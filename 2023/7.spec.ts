9cac024860c3160ba7096a0cefde99fc8591ef68
Max Carroll <max.carroll@sky.uk>
2023-12-07 07:02:45 +0000

    7 first commit

---
+++ 2023/7.spec.ts
@@ -0,0 +1,29 @@
+import assert from "node:assert";
+import test from "node:test";
+import { Hand, getHandInfo } from "./7";
+
+test("should determine a 5 of a kind ", () => {
+  const hand = getHandInfo("AAAAA");
+
+  assert.equal(hand, Hand.fiveOfAKind);
+});
+
+test("should determine a 4 of a kind ", () => {
+  const hand = getHandInfo("AAAA4");
+
+  assert.equal(hand, Hand.fourOfAKind);
+
+  const hand2 = getHandInfo("A4AAA");
+
+  assert.equal(hand2, Hand.fourOfAKind);
+
+  const hand3 = getHandInfo("4AAAA");
+
+  assert.equal(hand3, Hand.fourOfAKind);
+});
+
+test("should determine a full house ", () => {
+  const hand = getHandInfo("AAA66");
+
+  assert.equal(hand, Hand.fullHouse);
+});
---
+++ 2023/7.ts
@@ -0,0 +1,51 @@
+export enum Hand {
+  highCard,
+  onePair,
+  twoPair,
+  threeOfAKind,
+  fullHouse,
+  fourOfAKind,
+  fiveOfAKind,
+}
+
+export function getHandInfo(hand: string): Hand {
+  const map = new Map<string, number>();
+
+  for (let card of hand) {
+    let currentNumber = map.get(card) ?? 0;
+    currentNumber++;
+
+    map.set(card, currentNumber);
+  }
+
+  const handKindMap = new Map<Hand, number>();
+
+  let hasA3 = false;
+  let numberOfPairs = 0;
+
+  let resultingHand: Hand = Hand.highCard;
+  for (let [card, qty] of map) {
+    switch (qty) {
+      case 1:
+        break;
+      case 2:
+        numberOfPairs++;
+        break;
+      case 3:
+        hasA3 = true;
+        break;
+      case 4:
+        resultingHand = Hand.fourOfAKind;
+        return resultingHand;
+      case 5:
+        resultingHand = Hand.fiveOfAKind;
+        return resultingHand;
+    }
+  }
+
+  if (hasA3 && numberOfPairs === 1) {
+    return Hand.fullHouse;
+  }
+
+  return resultingHand;
+}

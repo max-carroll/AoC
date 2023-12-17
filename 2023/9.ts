15eeeecb498a64540389f082e0e76405a9dc8a87
Max Carroll <max.carroll@sky.uk>
2023-12-17 17:33:23 +0000

    back on track

--- 2023/9.ts
+++ 2023/9.ts
@@ -3,44 +3,50 @@ export function getEndNumbers(input: string): Array<Array<number>> {
     .split("\n")
     .map((line) => line.split(" ").map((x) => parseInt(x)));
 
-  let lineIndex = 0;
-  let differenceFound = 0;
+  for (var sequence of matrix) {
+    const currentTriangle = [sequence];
 
-  while (true) {
-    // get top line of matrix
+    // calculate line differences untill theres no difference
+    while (true) {
+      let differences: Array<number> = [];
 
-    let differences: Array<number> = [];
+      // get the differences
+      for (var i = 0; i < currentTriangle[0].length - 1; i++) {
+        const diff = currentTriangle[0][i + 1] - currentTriangle[0][i];
 
-    const currentLine = matrix[lineIndex];
-    for (var i = 0; i < matrix[lineIndex].length - 1; i++) {
-      const diff = currentLine[i + 1] - currentLine[i];
-      differences.push(diff);
+        if (isNaN(diff)) throw new Error("NOT A NUMBER");
+
+        differences.push(diff);
+      }
+      // put them at the top of the triangle
+      currentTriangle.unshift(differences);
+      if (differences.every((d) => d === differences[0])) {
+        break;
+      }
     }
-    matrix.unshift(differences);
-    if (differences.every((d) => d === differences[0])) {
-      differenceFound = differences[0];
-      break;
+
+    // add the zeros
+    const zeros: number[] = [];
+    for (var i = 0; i < currentTriangle[0].length - 1; i++) {
+      zeros.push(0);
     }
+    currentTriangle.unshift(zeros);
+
+    // calculate the end number on each line of the triangle
+    for (
+      var lineNumber = 1;
+      lineNumber < currentTriangle.length;
+      lineNumber++
+    ) {
+      const previousLastNumber = currentTriangle[lineNumber - 1].at(-1)!;
+      const currentLastOneInSequence = currentTriangle[lineNumber].at(-1)!;
+      const newNumberInSequence = previousLastNumber + currentLastOneInSequence;
+
+      currentTriangle[lineNumber].push(newNumberInSequence);
+    }
+    console.log("printing triangle");
+    console.log(currentTriangle);
   }
-
-  const zeros: number[] = [];
-  for (var i = 0; i < matrix[0].length - 1; i++) {
-    zeros.push(0);
-  }
-  matrix.unshift(zeros);
-
-  // add on the line numbers
-  for (var lineNumber = 1; lineNumber <= matrix.length; lineNumber++) {
-    const previousLastNumber = matrix[lineNumber - 1].at(-1)!;
-    const currentLastOneInSequence = matrix[lineNumber].at(-1)!;
-    const newNumberInSequence = previousLastNumber + currentLastOneInSequence;
-
-    matrix[lineNumber].push(newNumberInSequence);
-
-    console.log(newNumberInSequence);
-  }
-
-  console.log(matrix);
 
   return matrix;
 }

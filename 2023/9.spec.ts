feb3c0858f50942b79a8cecc5816f3247c8e749f
Max Carroll <max.carroll@sky.uk>
2023-12-17 17:46:27 +0000

    yay thats part1

--- 2023/9.spec.ts
+++ 2023/9.spec.ts
@@ -1,12 +1,18 @@
 import assert from "node:assert";
 import test from "node:test";
 import { getEndNumbers } from "./9";
+import { realInputData } from "./9.raw";
 
 const testInput = `0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45`;
 
-test("should ", () => {
+test("should do stuff ", () => {
   const result = getEndNumbers(testInput);
-  assert.equal(result, "hello");
+  assert.equal(result, 114);
+});
+
+test("should do stuff - part1 ", () => {
+  const result = getEndNumbers(realInputData);
+  assert.equal(result, 1834108701); // guess 1 = 1834108701
 });
--- 2023/9.ts
+++ 2023/9.ts
@@ -1,54 +1,72 @@
-export function getEndNumbers(input: string): Array<Array<number>> {
+export function getEndNumbers(input: string): number {
   const matrix = input
     .split("\n")
     .map((line) => line.split(" ").map((x) => parseInt(x)));
 
+  const results: any = [];
+  let indexOfSequenceInMatrix = 0;
+
+  let actualRealAnswer = 0;
   for (var sequence of matrix) {
-    const currentTriangle = [sequence];
-
-    // calculate line differences untill theres no difference
-    while (true) {
-      let differences: Array<number> = [];
-
-      // get the differences
-      for (var i = 0; i < currentTriangle[0].length - 1; i++) {
-        const diff = currentTriangle[0][i + 1] - currentTriangle[0][i];
-
-        if (isNaN(diff)) throw new Error("NOT A NUMBER");
-
-        differences.push(diff);
-      }
-      // put them at the top of the triangle
-      currentTriangle.unshift(differences);
-      if (differences.every((d) => d === differences[0])) {
-        break;
-      }
-    }
-
-    // add the zeros
-    const zeros: number[] = [];
-    for (var i = 0; i < currentTriangle[0].length - 1; i++) {
-      zeros.push(0);
-    }
-    currentTriangle.unshift(zeros);
-
-    // calculate the end number on each line of the triangle
-    for (
-      var lineNumber = 1;
-      lineNumber < currentTriangle.length;
-      lineNumber++
-    ) {
-      const previousLastNumber = currentTriangle[lineNumber - 1].at(-1)!;
-      const currentLastOneInSequence = currentTriangle[lineNumber].at(-1)!;
-      const newNumberInSequence = previousLastNumber + currentLastOneInSequence;
-
-      currentTriangle[lineNumber].push(newNumberInSequence);
-    }
-    console.log("printing triangle");
-    console.log(currentTriangle);
+    const triangle = getCurrentTriangle(sequence, indexOfSequenceInMatrix);
+    results.push(triangle);
+    actualRealAnswer += triangle.finalNumber;
+    indexOfSequenceInMatrix++;
   }
 
-  return matrix;
+  console.table(results);
+  console.log({ actualRealAnswer });
+
+  return actualRealAnswer;
+}
+
+function getCurrentTriangle(
+  sequence: number[],
+  indexOfSequenceInMatrix: number
+) {
+  const currentTriangle = [sequence];
+
+  // calculate line differences untill theres no difference
+  while (true) {
+    let differences: Array<number> = [];
+
+    // get the differences
+    for (var i = 0; i < currentTriangle[0].length - 1; i++) {
+      const diff = currentTriangle[0][i + 1] - currentTriangle[0][i];
+
+      if (isNaN(diff)) throw new Error("NOT A NUMBER");
+
+      differences.push(diff);
+    }
+    // put them at the top of the triangle
+    currentTriangle.unshift(differences);
+    if (differences.every((d) => d === differences[0])) {
+      break;
+    }
+  }
+
+  // add the zeros
+  const zeros: number[] = [];
+  for (var i = 0; i < currentTriangle[0].length - 1; i++) {
+    zeros.push(0);
+  }
+  currentTriangle.unshift(zeros);
+
+  // calculate the end number on each line of the triangle
+  for (var lineNumber = 1; lineNumber < currentTriangle.length; lineNumber++) {
+    const previousLastNumber = currentTriangle[lineNumber - 1].at(-1)!;
+    const currentLastOneInSequence = currentTriangle[lineNumber].at(-1)!;
+    const newNumberInSequence = previousLastNumber + currentLastOneInSequence;
+
+    currentTriangle[lineNumber].push(newNumberInSequence);
+  }
+  const finalNumber: number = currentTriangle.at(-1)?.at(-1)!;
+  return {
+    indexOfSequenceInMatrix,
+    // currentTriangle,
+    startingNumbers: JSON.stringify(currentTriangle.at(-1)),
+    finalNumber,
+  };
 }
 
 // 21
